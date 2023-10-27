645ca322d383acf2b4dd3a7a5946e4273e94538f
s-yogeshwaran <95536746+s-yogeshwaran@users.noreply.github.com>
2023-10-27 21:26:34 +0530

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+
+# set web page
+st.set_page_config(page_title="DV Credit Card Fraud Detection",
+                   page_icon="🕵🏼‍♀️", #💳
+                   initial_sidebar_state="expanded",
+                   layout= "wide")
+
+# html_temp = """
+# <div style=" text-align:center;">
+# <h4 style=" font-size: 20px;">SRM Internship Project</h4>
+# </div>
+# """
+# st.sidebar.markdown(html_temp, unsafe_allow_html=True)
+
+
+# title of the body
+html_temp = """
+<div style="-moz-box-shadow: 1px 1px 3px 2px red;
+  -webkit-box-shadow: 1px 1px 3px 2px red;
+  box-shadow: 1px 1px 3px 2px red;
+  padding: 10px;
+  font-size: 30px;
+  font-weight: bold;
+  text-align: center;
+  font-family: Helvetica
+">
+SRM Internship Project <br>
+Credit Card Fraud Detection
+</div>"""
+st.markdown(html_temp, unsafe_allow_html=True)
+
+
+# title of the sidebar
+html_temp = """
+<div style="background-color:red">
+<p style="color:white;
+            text-align:center;
+            font-size: 17px;
+            ">
+Select Your Data </p>
+</div>"""
+st.sidebar.markdown(html_temp,unsafe_allow_html=True)
+
+
+
+
+
+
