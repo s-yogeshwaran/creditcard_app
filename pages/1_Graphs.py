80d608cf315ce295e0f868faa4ae2dd1620ef18d
s-yogeshwaran <95536746+s-yogeshwaran@users.noreply.github.com>
2023-10-27 14:30:33 +0530

    Update 1_Graphs.py

--- pages/1_Graphs.py
+++ pages/1_Graphs.py
@@ -83,7 +83,7 @@ hist_data = [class_0, class_1]
 group_labels = ['Legit', 'Fraud']
 
 fig = ff.create_distplot(hist_data, group_labels, show_hist=False, show_rug=False)
-fig['layout'].update(title='Credit Card Transactions Time Density Plot', align = 'center', xaxis=dict(title='Time [s]'))
+fig['layout'].update(title='Credit Card Transactions Time Density Plot', xaxis=dict(title='Time [s]'))
 st.plotly_chart(fig)
 # iplot(fig, filename='dist_only')
 st.write("Fraudulent transactions have a distribution more even than valid transactions - are equaly distributed in time, including the low real transaction times, during night in Europe timezone.")
@@ -111,7 +111,7 @@ fig, (ax1, ax2) = plt.subplots(ncols=2)
 s = sns.lineplot(ax = ax1, x="Hour", y="Transactions", data=df.loc[df.Class==0])
 s = sns.lineplot(ax = ax2, x="Hour", y="Transactions", data=df.loc[df.Class==1], color="red")
 
-plt.suptitle("Total Number of Transactions", fontsize = 18)
+plt.title("Total Number of Transactions", fontsize = 18)
 st.plotly_chart(fig)
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -131,7 +131,7 @@ fig, (ax1, ax2) = plt.subplots(ncols=2)
 s = sns.lineplot(ax = ax1, x="Hour", y="Max", data=df.loc[df.Class==0])
 s = sns.lineplot(ax = ax2, x="Hour", y="Max", data=df.loc[df.Class==1], color="red")
 
-plt.suptitle("Maximum Amount of Transactions", fontsize = 18)
+plt.title("Maximum Amount of Transactions", fontsize = 18)
 st.plotly_chart(fig)
 
 #----------------------------------------------------------------------------------------------------------------------------------------
